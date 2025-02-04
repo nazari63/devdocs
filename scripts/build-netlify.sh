76d58c1ae9d83c33877f227cefe825e419fe1a3a
Matthew Slipper <me@matthewslipper.com>
2025-02-04 11:11:15 -0700

    Update PATH and mdbook location

--- scripts/build-netlify.sh
+++ scripts/build-netlify.sh
@@ -7,6 +7,7 @@ rustup default stable
 
 echo "Installing mdbook..."
 curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.44/mdbook-v0.4.44-x86_64-unknown-linux-gnu.tar.gz | tar xvz
+mv mdbook /usr/local/bin
 
 echo "Installing mdbook-mermaid.."
 cargo install mdbook-mermaid
@@ -15,4 +16,5 @@ echo "Installing uv..."
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 echo "Building..."
+export PATH="$HOME/.cargo/bin:$PATH"
 uv run run.py
